0e361f9ef222f50df801b678af1e0da5ac4bfb62
J.D. Warren <johndavid400@gmail.com>
2014-08-10 10:35:23 -0500

    updated lib

---
+++ lib/forem-bootstrap.rb
@@ -0,0 +1,8 @@
+require 'forem'
+require 'foundation-rails'
+module Forem
+  module Foundation
+    class Engine < Rails::Engine
+    end
+  end
+end
---
+++ lib/forem-bootstrap/version.rb
@@ -0,0 +1,5 @@
+module Forem
+  module Foundation
+    VERSION = "0.0.1"
+  end
+end
